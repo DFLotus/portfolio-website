5b8533f3ac570b604531b3d6e2e785c0c29d13e9
DFLotus <deondraefrancis101@gmail.com>
2025-06-13 17:36:49 -0400

    Theme logic

--- ps-site/src/app/app.ts
+++ ps-site/src/app/app.ts
@@ -8,5 +8,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'ps-site';
+  isDarkMode: boolean = false;
+  iconPath: string = "../assets/dark_mode_moon_icon.png";
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+
+    const body: any = document.body;
+
+    if (this.isDarkMode) {
+      //Make it dark mode
+      body.classList.add('dark-theme');
+      this.iconPath = "../assets/light_mode_sun_icon.png";
+    }
+    else {
+      body.classList.remove('dark-theme');
+      this.iconPath = "../assets/dark_mode_moon_icon.png";
+    }
+
+  }
+
 }
